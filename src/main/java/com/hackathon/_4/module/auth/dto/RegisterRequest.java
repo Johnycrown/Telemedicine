7a1085682d79616b9a552d94a3d407254e3b52e5
John <john.oladeji@slsbank.com>
2024-06-16 06:12:30 +0100

    created the user registration request

---
+++ src/main/java/com/hackathon/_4/module/auth/dto/RegisterRequest.java
@@ -0,0 +1,19 @@
+package com.hackathon._4.module.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private Role role;
+    private boolean mfaEnabled;
+}
