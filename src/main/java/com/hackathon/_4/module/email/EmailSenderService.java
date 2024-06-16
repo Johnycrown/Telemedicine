b2d64825f3f01b57be6981bd63599b923f0848cd
John <john.oladeji@slsbank.com>
2024-06-16 05:52:45 +0100

    implemented email configuration

---
+++ src/main/java/com/hackathon/_4/module/email/EmailSenderService.java
@@ -0,0 +1,36 @@
+package com.hackathon._4.module.email;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+
+
+@Component
+public class EmailSenderService {
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void sendMailWithAttachment(String toEmail,
+                                       String body,
+                                       String subject,
+                                       byte[] excelBytes) throws MessagingException {
+        MimeMessage mimeMessage=javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper=new MimeMessageHelper(mimeMessage,true);
+        mimeMessageHelper.setFrom("arjungautam8877@gmail.com");
+        mimeMessageHelper.setTo(toEmail);
+        mimeMessageHelper.setText(body);
+        mimeMessageHelper.setSubject(subject);
+
+
+        mimeMessageHelper.addAttachment("data.xlsx", new ByteArrayResource(excelBytes));
+
+        javaMailSender.send(mimeMessage);
+        System.out.printf("Mail with attachment sent successfully..");
+
+    }
+}
