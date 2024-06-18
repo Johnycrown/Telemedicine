0a8d8a45dbf491053ad423f8e79c8ed98004250d
John <john.oladeji@slsbank.com>
2024-06-18 15:15:50 +0100

    Fixed cors issues

--- src/main/java/com/telemedicine/module/auth/controller/AuthenticationController.java
+++ src/main/java/com/telemedicine/module/auth/controller/AuthenticationController.java
@@ -10,21 +10,20 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1/auth")
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 @Slf4j
 public class AuthenticationController {
     private final AuthenticationService service;
 
     @PostMapping("/register")
+    @CrossOrigin(origins = "*")
     public ResponseEntity<?> register(
             @RequestBody RegisterRequest request
     ) {try {
